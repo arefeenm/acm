63254e254759b2a1bf0583f3fbbcc76de1b0fd58
arefeenm <arefeen@newscred.com>
2019-12-05 22:25:04 +0600

    test commit

---
+++ rough/cmake-build-debug/CMakeFiles/rough.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rough.dir/rough_101.cpp.o"
+  "rough.pdb"
+  "rough"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rough.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
